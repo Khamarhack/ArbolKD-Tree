651f98cf9e42df828fc459a26359a0647cf0ebe9
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2024-09-25 20:06:50 -0500

    Update kdtree.hxx

--- Arbol KD-Tree/kdtree.hxx
+++ Arbol KD-Tree/kdtree.hxx
@@ -3,105 +3,99 @@
 
 using namespace std;
 
-
 template<class T>
-kdtree<T>::kdtree(){
-	this->raiz=NULL;
-}
-
-
-template<class T>
-bool kdtree<T>::esVacio()
-{
-   if(this==NULL)
-   {
-      return true;
-   }
-   else
-   {
-     return false;
-   }
+kdtree<T>::kdtree() {
+    this->raiz = NULL;
 }
 
 template<class T>
-T& kdtree<T>::datoRaiz()
-{
-   return this->raiz->obtenerDato();
+bool kdtree<T>::esVacio() {
+    return this->raiz == NULL;
 }
 
 template<class T>
-int kdtree<T>::altura()
-{
-   return this->raiz->altura();
+vector<T>& kdtree<T>::datoRaiz() {
+    if (this->raiz != NULL) {
+        return this->raiz->obtenerDato();
+    }
+    throw runtime_error("El árbol está vacío.");
 }
 
 template<class T>
-int kdtree<T>::tamano()
-{
-	return this->raiz->tamano() ;
+int kdtree<T>::altura() {
+    return this->raiz ? this->raiz->altura() : 0;
 }
 
 template<class T>
-void kdtree<T>::insertar(T& val)
-{
-	if(this->raiz!=NULL){
-		this->raiz->insertar(val);
-	}
-	else{
-		kdnodo<T>* nuevo= new kdnodo<T>();
-		nuevo->fijarDato(val);
-		this->raiz=nuevo;
-	}
-
-
-}
-
-
-
-template<class T>
-bool kdtree<T>::eliminar(T& val)
-{
-
+int kdtree<T>::tamano() {
+    return this->raiz ? this->raiz->tamano() : 0;
 }
 
 template<class T>
-kdnodo<T>* kdtree<T>::buscar(T& val)
-{
-	return this->raiz->buscar(val);
+void kdtree<T>::insertar(vector<T>& val) {
+    if (this->raiz != NULL) {
+        this->raiz->insertar(val);
+    } else {
+        kdnodo<T>* nuevo = new kdnodo<T>();
+        nuevo->fijarDato(val);
+        nuevo->fijarTag(0); // Asignar el tag inicial
+        this->raiz = nuevo;
+    }
 }
 
 template<class T>
-void kdtree<T>::preOrden()
-{
-	this->raiz->preOrden();
+bool kdtree<T>::eliminar(vector<T>& val) {
+    // Implementar la lógica de eliminación aquí
+    return false; // Placeholder
 }
 
 template<class T>
-void kdtree<T>::inOrden()
-{
-	this->raiz->inOrden();
+kdnodo<T>* kdtree<T>::buscar(vector<T>& val) {
+    if (this->raiz != NULL) {
+        return this->raiz->buscar(val);
+    }
+    return NULL; // Árbol vacío
 }
 
 template<class T>
-void kdtree<T>::posOrden()
-{
-	this->raiz->posOrden();
+void kdtree<T>::preOrden() {
+    if (this->raiz != NULL) {
+        this->raiz->preOrden();
+    }
 }
 
 template<class T>
-void kdtree<T>::nivelOrden()
-{
-	this->raiz->nivelOrden();
+void kdtree<T>::inOrden() {
+    if (this->raiz != NULL) {
+        this->raiz->inOrden();
+    }
 }
 
 template<class T>
-void kdtree<T>::maximo(int &maxi)
-{
-	this->raiz->maximo(maxi);
+void kdtree<T>::posOrden() {
+    if (this->raiz != NULL) {
+        this->raiz->posOrden();
+    }
 }
 
 template<class T>
-void kdtree<T>::minimo(int &mini)
-{
-	this->raiz->minimo(mini);
+void kdtree<T>::nivelOrden() {
+    if (this->raiz != NULL) {
+        this->raiz->nivelOrden();
+    }
 }
+
+template<class T>
+void kdtree<T>::maximo(int& maxi) {
+    if (this->raiz != NULL) {
+        this->raiz->maximo(maxi);
+    }
+}
+
+template<class T>
+void kdtree<T>::minimo(int& mini) {
+    if (this->raiz != NULL) {
+        this->raiz->minimo(mini);
+    }
+}
+
